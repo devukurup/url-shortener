f950e3f1eafdb3d7443d4db154dbf2f3af6a1bcd
Devu R Kurup <40559138+devukurup@users.noreply.github.com>
2023-05-31 02:06:07 +0530

    Defined home route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
+  get "*path", to: "home#index", via: :all
 end
