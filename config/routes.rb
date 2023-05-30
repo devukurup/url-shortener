d63fb6a1d9b248e7f9bb37589707624b1d113b28
Devu R Kurup <40559138+devukurup@users.noreply.github.com>
2023-05-31 04:14:18 +0530

    Created a redirects controller and added a route to redirect the shortened url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :links, only: %i[show new create]
+  get ':slug', to: 'redirects#show', slug: /[0-9a-fA-F]+/
   
-  root "home#index"
-  get "*path", to: "home#index", via: :all
+  root 'home#index'
+  get '*path', to: 'home#index', via: :all
 end
