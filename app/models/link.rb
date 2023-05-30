09c62805a5acf819f95c0e2642b271651fc87206
Devu R Kurup <40559138+devukurup@users.noreply.github.com>
2023-05-31 02:18:24 +0530

    Added a model method to set slug

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,18 @@
 class Link < ApplicationRecord
   validates :url, presence: true
-  validates :slug, uniqueness: true, presence: true
+  validates :slug, uniqueness: true
+
+  before_create :set_slug
+
+  private
+
+    def set_slug
+      loop do
+        new_slug = SecureRandom.hex(6)
+        unless Link.where(slug: new_slug).exists?
+          self.slug = new_slug
+          break
+        end
+      end
+    end
 end
